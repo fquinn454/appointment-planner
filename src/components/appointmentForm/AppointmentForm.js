e7c5ec1e7b65bc45d295da4a43054c89c65569b5
Fiona <57137659+fquinn454@users.noreply.github.com>
2023-04-24 11:12:32 +0100

    all features working

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,11 +1,5 @@
 import React from "react";
-
-const getTodayString = () => {
-  const [month, day, year] = new Date()
-    .toLocaleDateString("en-US")
-    .split("/");
-  return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
-};
+import { ContactPicker } from "../contactPicker/ContactPicker";
 
 export const AppointmentForm = ({
   contacts,
@@ -19,8 +13,62 @@ export const AppointmentForm = ({
   setTime,
   handleSubmit
 }) => {
+  const getTodayString = () => {
+    const [month, day, year] = new Date()
+      .toLocaleDateString("en-US")
+      .split("/");
+    return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
+  };
+  
+  const getContactNames = () => {
+    return contacts.map((contact) => contact.name);
+  };
 
   return (
-    <></>
+    <form onSubmit={handleSubmit}>
+    <label>
+      <input
+        type="text"
+        name="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        required
+        placeholder="Appointment Title"
+      />
+    </label>
+    <br />
+    <label>
+      <ContactPicker
+        name="contact"
+        value={contact}
+        contacts={getContactNames()}
+        onChange={(e) => setContact(e.target.value)}
+        placeholder="Appointment With"
+      />
+    </label>
+    <br />
+    <label>
+      <input
+        type="date"
+        name="date"
+        min={getTodayString()}
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+        required
+      />
+    </label>
+    <br />
+    <label>
+      <input
+        type="time"
+        name="time"
+        value={time}
+        onChange={(e) => setTime(e.target.value)}
+        required
+      />
+    </label>
+    <br />
+    <input type="submit" value="Add Appointment" />
+  </form>
   );
 };
--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,7 +10,44 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    <></>
+    <form onSubmit={handleSubmit}>
+    <label>
+      <input
+        type="text"
+        name="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required
+        placeholder="Contact Name"
+      />
+    </label>
+    <br />
+    <label>
+      <input
+        type="tel"
+        name="phone"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+        required
+        // regex is for UK mobile numbers
+        pattern="[0][7-9]{2} [1-9][0-9]{7}"
+        placeholder="Contact Phone (0## #######)"
+      />
+    </label>
+    <br />
+    <label>
+      <input
+        type="email"
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+        placeholder="Contact Email"
+      />
+    </label>
+    <br />
+    <input type="submit" value="Add Contact" />
+  </form>
   );
 };
 
